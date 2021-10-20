b082223eeca57565d796bde6bd60f99ead517bec
mlvl974 <84314421+mlvl974@users.noreply.github.com>
2021-10-21 00:24:55 +0800

    Upload Vaccination updated

--- screens/UploadVaccination.js
+++ screens/UploadVaccination.js
@@ -1,11 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Button, Platform, Text, Image,TouchableOpacity,ToastAndroid, ScrollView } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, picker, SafeAreaView, ScrollView, Button} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 import * as DocumentPicker from 'expo-document-picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+import RadioForm, {
+  RadioButton, 
+  RadioButtonInput, 
+  RadioButtonLabel
+} from 'react-native-simple-radio-button';
+
 import firebase from 'firebase';
 import "firebase/firestore";
 import 'firebase/firebase-storage';
 
 const UploadVaccinationScreen = () =>{
+
+  const [dose, setDose]= useState('Pfizer')
+  const [type, setType]= useState('Pfizer')
+
+  var result = [
+    {label: "Yes", value: 'yes'},
+    {label: "No", value: 'no'},
+  ];
+  const [testResult, setTestResult]=useState('yes');
+  const [selectedImage, setSelectedImage] = useState('');
+
+
+
+
+
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const [text, setText] = useState('Empty')
+  
+
+  const onChangeDate = (event, selectedDate) => {
+    setShow(false)
+    const currentDate = selectedDate || date;
+  
+    setDate(currentDate);
+
+    // Process the date values
+    let tempDate = new Date(currentDate);
+    let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
+    setText(fDate)
+
+    // Log the Date values
+    console.log(tempDate)
+    
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
 
   const [selectedDocument, setSelectedDocument] = useState();
   const [docName, setDocName] = useState('None');
@@ -83,19 +134,102 @@ const UploadVaccinationScreen = () =>{
 
   }
 
+    //save art date and download link
+    const saveUploadData = (downloadURL) => {
+
+      firebase.firestore()
+          .collection('organisations')
+          .doc(orgId)
+          .collection("employees")
+          .doc(userId)
+          .update({
+              "ARTDate" : date,
+              "ARTResult" : downloadURL,
+          }).then((function () {
+              alert('ART results successfully uploaded!')
+          }))
+
+  };
 
   return (
     <View style={styles.container}>
+       
+    <Text style={styles.questionText}> Are you Vaccinated?   </Text>
 
-    <Image source={require("../assets/logo.png")} style={styles.logo}/>
- 
- 
-    <Text style={styles.headerText}> Vaccination Status</Text>
+        <RadioForm
+              radio_props={result}
+              initial={2}
+              onPress={(value) => {setTestResult(value)}}
+              buttonSize={10}
+              buttonOuterSize={20}
+              selectedButtonColor={'white'}
+              selectedLabelColor={'white'}
+              labelStyle={{ fontSize: 20, }}
+              disabled={false}
+              formHorizontal={false}
+            />
+
+<Text style={styles.questionText}> Vaccine Type?  </Text>
+
+<View style={styles.container}>
+<Picker
+
+                 selectedValue={type}
+                 style={styles.picker}
+                 value={type}
+                 onValueChange={(value, index)=>setType(value)}
+             >
+                 <Picker.item label="Pfizer" value="Pfizer"/>
+                 <Picker.item label="Moderna" value="Moderna"/>
+                 <Picker.item label="Sinovac" value="Sinovac"/>
+
+             </Picker>
+
+
+
+             
+      </View>
+
+      <Text style={styles.questionText}> No. of doses taken </Text>
+
+<View style={styles.container}>
+<Picker
+                 selectedValue={dose}
+                 style={styles.picker}
+                 value={dose}
+                 onValueChange={(value, index)=>setDose(value)}
+             >
+                 <Picker.item label="1" value="1"/>
+                 <Picker.item label="2" value="2"/>
+                 <Picker.item label="3" value="3"/>
+
+             </Picker>
+      </View>
+
+
+    <Text style={styles.questionText}> Please enter date of latest dose.  </Text>
+
+    <Text style={{ fontWeight: 'bold', fontSize: 14 }}>{text}</Text>
+    <View style={{ margin: 10 }}>
+      <Button onPress={() => showMode('date')} title="Select Date" />
+      </View>
+          
+          {show && (
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode={mode}
+              is24Hour={true}
+              display="default"
+              onChange={onChangeDate} 
+            />
+          )}
 
  <Text style={styles.questionText}> Please Upload Certificate.</Text>
 
 <View style={styles.container}>
 <Text>Selected File: {docName}</Text>
+<View style={{ margin: 10 }}></View>
 <Button title="Upload Document" onPress={pickDocument}/>
 </View>
  <View style={styles.buttonContainer}>
@@ -109,33 +243,41 @@ const UploadVaccinationScreen = () =>{
 export default UploadVaccinationScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#51a4fb',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: '100%',
-    height: '20%',
-    resizeMode: 'contain',
-  },
 
-  headerText: {
-    color: "#555555",
-    fontSize: 24,
-    marginBottom: 10,
-},
+    container: {
+        flex: 1,
+        backgroundColor: '#d4e9ff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+
+
 
   questionText: {
-    color: "#EEEEEE",
-    marginTop: 14,
-    marginBottom: 10,
+    color: "#000000",
+    marginTop: 12,
+    marginBottom: 8,
     fontSize: 18,
+  
 },
 
   buttonContainer: {
-    marginBottom: 50,
-  }
+    marginTop: 30,
+    marginBottom: 30,
+  },
+
+  text: {
+    marginTop: 10,
+    marginLeft: 10,
+},
+
+    picker: {
+        width: 150,
+        height: 30,
+        borderColor:'blue',
+        color: 'red',
+       
+    }
 
 });

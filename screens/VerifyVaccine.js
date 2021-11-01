1584f9f1166943ff13b4824d534f0270b36aa9a0
mlvl974 <84314421+mlvl974@users.noreply.github.com>
2021-11-02 02:40:20 +0800

    Update VerifyVaccine.js

--- screens/VerifyVaccine.js
+++ screens/VerifyVaccine.js
@@ -5,12 +5,21 @@ import { DataTable } from 'react-native-paper';
 
 const VerifyVaccineScreen = ({navigation}) => {
   const [tableData, setTableData] = useState([
-    { name:"Mary Tan", id: 12388, department:"Finance", vaccinated:"Yes", num: 2, date:"08/08/2021", certificate: "vaccine1.jpg"   },
-    { name:"John Jones", id: 12399, department:"HR", vaccinated:"Yes", num: 2, date:"09/09/2021", certificate: "vaccine1.jpg"   },
-    { name:"Bruce Wayne", id: 12377, department:"Logistics", vaccinated:"No", num: 2, date:"07/07/2021", certificate: "vaccine1.jpg"   },
-    { name:"Tony Stark", id: 12366, department:"Research", vaccinated:"Yes", num: 3, date:"06/06/2021", certificate: "vaccine1.jpg"   },
+    { id: 12388, name:"Mary Tan", department:"Finance", vaccinated:"Yes", num: 2, date:"08/08/2021", certificate: "vaccine1.jpg"   },
+    {  id: 12399, name:"John Jones", department:"HR", vaccinated:"Yes", num: 2, date:"09/09/2021", certificate: "vaccine1.jpg"   },
+    { id: 12377, name:"Bruce Wayne", department:"Logistics", vaccinated:"No", num: 2, date:"07/07/2021", certificate: "vaccine1.jpg"   },
+    { id: 12366, name:"Tony Stark",  department:"Research", vaccinated:"Yes", num: 3, date:"06/06/2021", certificate: "vaccine1.jpg"   },
     
   ])
+
+  const [filteredData, setFilteredData] = useState([
+    { id: 12388, name:"Mary Tan", department:"Finance", vaccinated:"Yes", num: 2, date:"08/08/2021", certificate: "vaccine1.jpg"   },
+    {  id: 12399, name:"John Jones", department:"HR", vaccinated:"Yes", num: 2, date:"09/09/2021", certificate: "vaccine1.jpg"   },
+    { id: 12377, name:"Bruce Wayne", department:"Logistics", vaccinated:"No", num: 2, date:"07/07/2021", certificate: "vaccine1.jpg"   },
+    { id: 12366, name:"Tony Stark",  department:"Research", vaccinated:"Yes", num: 3, date:"06/06/2021", certificate: "vaccine1.jpg"   },
+    
+  ])
+
 
   const searchEmployeeID = (text) => {
     if (text) {
@@ -40,6 +49,81 @@ const VerifyVaccineScreen = ({navigation}) => {
     }
   }
 
+
+  const searchEmployeeDepartment = (text) => {
+    if (text) {
+      const newData = tableData.filter((item) => {
+        const itemData = item.department ? item.department.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+    }
+    else {
+      setFilteredData(tableData);
+    }
+  }
+
+  const searchEmployeeVaccinated = (text) => {
+    if (text) {
+      const newData = tableData.filter((item) => {
+        const itemData = item.vaccinated ? item.vaccinated.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+    }
+    else {
+      setFilteredData(tableData);
+    }
+  }
+
+      const searchEmployeeNum = (text) => {
+    if (text) {
+      const newData = tableData.filter((item) => {
+        const itemData = item.num ? item.num.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+    }
+    else {
+      setFilteredData(tableData);
+    }
+  }
+
+
+    const searchEmployeeDate = (text) => {
+    if (text) {
+      const newData = tableData.filter((item) => {
+        const itemData = item.date ? item.date.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+    }
+    else {
+      setFilteredData(tableData);
+    }
+  }
+
+      const searchEmployeeCertificate = (text) => {
+    if (text) {
+      const newData = tableData.filter((item) => {
+        const itemData = item.certificate ? item.certificate.toUpperCase() : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+    }
+    else {
+      setFilteredData(tableData);
+    }
+  }
+
+
+
+
   return (
     <SafeAreaView>
     <ScrollView>
@@ -54,6 +138,29 @@ const VerifyVaccineScreen = ({navigation}) => {
             placeholder="Name"
             style={{padding: 2.5}}
             onChangeText={(text) => searchEmployeeName(text)} /></DataTable.Title>
+            <DataTable.Title><TextInput
+            placeholder="Department"
+            style={{padding: 2.5}}
+            onChangeText={(text) => searchEmployeeDepartment(text)} /></DataTable.Title>
+            <DataTable.Title><TextInput
+            placeholder="Vaccine Status"
+            style={{padding: 2.5}}
+            onChangeText={(text) => searchEmployeeVaccinated(text)} /></DataTable.Title>
+            <DataTable.Title><TextInput
+            placeholder="No. of Dose"
+            style={{padding: 2.5}}
+            onChangeText={(text) => searchEmployeeNum(text)} /></DataTable.Title>
+            <DataTable.Title><TextInput
+            placeholder="Date"
+            style={{padding: 2.5}}
+            onChangeText={(text) => searchEmployeeDate(text)} /></DataTable.Title>
+            <DataTable.Title><TextInput
+            placeholder="Certificate"
+            style={{padding: 2.5}}
+            onChangeText={(text) => searchEmployeeCertificate(text)} /></DataTable.Title>
+       
+       
+       
        
         </DataTable.Header>
 
@@ -62,6 +169,7 @@ const VerifyVaccineScreen = ({navigation}) => {
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => (
             <DataTable.Row>
+              <DataTable.Cell>{item.id}</DataTable.Cell>
               <DataTable.Cell><TextInput
                 style={{padding: 5}}
                 value={item.name}
